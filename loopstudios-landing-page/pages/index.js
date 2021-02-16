2edf15626c11491b445e7156331bf1a36fa907bd
Mahabubur Rahman <microronyy@gmail.com>
2021-02-16 19:43:52 +0600

    set up components and images

--- loopstudios-landing-page/pages/index.js
+++ loopstudios-landing-page/pages/index.js
@@ -1,12 +1,20 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import VR from '../components/VR'
+import Creations from '../components/Creations'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Loop Studio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <VR />
+      <Creations />
+      <Footer />
     </div>
   )
 }
